84f8a0b40ce107a76d288e53cc22b87eb68f693f
NakRex <khannasimahmad@gmail.com>
2021-12-18 23:36:20 +0530

    added user schema

---
+++ Backend/model/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+let UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email :{
+        type:String,
+        required: true
+    },
+    password: {
+        type:password,
+        required: true
+    },
+    date: {
+        type:Date,
+        default: Date.now
+    }
+});
+let User = mongoose.model("User", UserSchema);
+module.exports=User;
