57762a76abe5f70a5e0ac337ca73d7e806b27ec3
NakRex <khannasimahmad@gmail.com>
2021-12-21 17:33:17 +0530

    added login function

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const User = require('../model/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const secretKey = require('../config/jwtconfig')
 
-exports.register = (req, res)=>{
+exports.signUp = (req, res)=>{
     let user = new User({
         name:req.body.name,
         email: req.body.email,
@@ -16,4 +18,18 @@ exports.register = (req, res)=>{
         }
     });
     
+};
+
+exports.login = async (req, res)=>{
+    const {email, password} = req.body;
+    if(!(email&&password)){
+        res.status(400).send("All input is required");
+    }
+    const user = await User.findOne({ email });
+    if(user && (bcrypt.compare(password, user.password))){
+        const token = jwt.sign({userId: user._id, email}, secretKey, {"expiresIn": "2h"});
+        user.token = token;
+        res.status(200).json(user)
+    }
+    res.status(400).send("invalid credential")
 }
