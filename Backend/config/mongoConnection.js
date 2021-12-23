541589ddbd83007c4dd7468b2a216f4af01e234c
NakRex <khannasimahmad@gmail.com>
2021-12-23 16:44:41 +0530

    added mogo connection and exported connection instance

---
+++ Backend/config/mongoConnection.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const config = require('./dbconfig')
+
+
+const CONNECTION_URL = `mongodb://${config.host}:${config.port}/${config.db}`
+
+const connection = mongoose.createConnection(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongo has connected succesfully')
+})
+mongoose.connection.on('reconnected', () => {
+  console.log('Mongo has reconnected')
+})
+mongoose.connection.on('error', error => {
+  console.log('Mongo connection has an error', error)
+  mongoose.disconnect()
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongo connection is disconnected')
+})
+
+module.exports = connection;
