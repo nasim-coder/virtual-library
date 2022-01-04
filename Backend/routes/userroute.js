7cf8df9bbb2f7da0330c824af33d8e0274480e20
NakRex <khannasimahmad@gmail.com>
2022-01-04 17:56:49 +0530

    deleted unnecessary routes, and protected with token

--- Backend/routes/userroute.js
+++ Backend/routes/userroute.js
@@ -6,10 +6,10 @@ const verifyToken = require('../middleware/jwt')
 
 userRouter.post('/register', controller.signUp);
 userRouter.post('/login', controller.login);
-// userRouter.get('/books', controller.showBooks);
+userRouter.get('/getbooks/:department/:org_id', verifyToken, acontroller.getBookByDepartment)
+userRouter.get('/getallbook/:org_id', verifyToken, acontroller.getAllBook)
 // userRouter.post('/changepassword', controller.changePassword);
 // userRouter.post('/logout', controller.logout);
-userRouter.get('/getbooks/:department/:org_id', acontroller.getBookByDepartment)
-userRouter.get('/getallbook/:org_id', verifyToken, acontroller.getAllBook)
+
 
 module.exports=userRouter;
