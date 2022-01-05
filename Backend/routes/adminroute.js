5df0e3554e2e158e5ab18064283eb9f177b245cc
NakRex <khannasimahmad@gmail.com>
2022-01-05 15:40:56 +0530

    added route to add notice

--- Backend/routes/adminroute.js
+++ Backend/routes/adminroute.js
@@ -11,7 +11,8 @@ adminrouter.get('/readBook/:id', controller.readBook);
 adminrouter.get('/downloadBook/:id', controller.downloadBook)
 adminrouter.get('/deleteBook/:id', controller.deleteBook);
 adminrouter.get('/getBookbyDepartment/:department/:org_id', controller.getBookByDepartment)
-adminrouter.get('/getallbook/:org_id', controller.getAllBook)
+adminrouter.get('/getallbook/:org_id', controller.getAllBook);
+adminrouter.post('/addNotice/', controller.addNotice)
 //adminrouter.post('/changePassword', controller.changePassword);
 //adminrouter.post('/logout', controller.logout);
 
